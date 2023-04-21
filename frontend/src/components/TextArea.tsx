47383580a2d6b06eef35b21cd91bc6dca74b91ab
JensForstmann <19289807+JensForstmann@users.noreply.github.com>
2023-04-21 22:58:22 +0200

    Fix text area missing props

--- frontend/src/components/TextArea.tsx
+++ frontend/src/components/TextArea.tsx
@@ -1,5 +1,6 @@
 import { Component, ComponentProps } from 'solid-js';
 
 export const TextArea: Component<ComponentProps<'textarea'>> = (props) => {
-	return <textarea class={(props.class || '') + ' w-full'} {...props}></textarea>;
+	props.class = props.class + ' w-full';
+	return <textarea {...props}></textarea>;
 };
