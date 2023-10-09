b4c43608d9b9a23377f463042658d7ec39147492
JensForstmann <19289807+JensForstmann@users.noreply.github.com>
2023-10-09 15:32:54 +0200

    Improve dialog for loading round backups

--- frontend/src/components/RoundBackups.tsx
+++ frontend/src/components/RoundBackups.tsx
@@ -1,25 +1,28 @@
-import { Component, createResource, For, Match, Switch } from 'solid-js';
+import { Component, createResource, createSignal, For, Match, Switch } from 'solid-js';
 import { IMatchResponse } from '../../../common';
 import { createFetcher } from '../utils/fetcher';
 import { mustConfirm } from '../utils/mustConfirm';
 import { ErrorComponent } from './ErrorComponent';
 import { Loader } from './Loader';
+import { SelectInput } from './SelectInput';
+import { t } from '../utils/locale';
 
 export const RoundBackups: Component<{
 	match: IMatchResponse;
 	onClose?: () => void;
 }> = (props) => {
 	const fetcher = createFetcher(props.match.tmtSecret);
+	const [selectedFile, setSelectedFile] = createSignal<string>();
 	const [roundBackups] = createResource(() =>
 		fetcher<{ latestFiles: string[]; total: number }>(
 			'GET',
 			`/api/matches/${props.match.id}/server/round_backups?count=15`
 		)
 	);
-	const loadBackup = async (file: string) => {
+	const loadBackup = async () => {
 		const success = await fetcher<boolean>(
 			'POST',
-			`/api/matches/${props.match.id}/server/round_backups/${file}`
+			`/api/matches/${props.match.id}/server/round_backups/${selectedFile()}`
 		);
 		if (success) {
 			props.onClose?.();
@@ -33,21 +36,29 @@ export const RoundBackups: Component<{
 			</Match>
 			<Match when={roundBackups()}>
 				{(roundBackups) => (
-					<ul>
-						<For each={roundBackups().latestFiles}>
-							{(file) => (
-								<li
-									class="break-words"
-									onClick={mustConfirm(() => loadBackup(file))}
-								>
-									{file.replace(
-										`round_backup_${props.match.id.toLowerCase()}_`,
-										''
-									)}
-								</li>
-							)}
-						</For>
-					</ul>
+					<>
+						<SelectInput
+							value={selectedFile()}
+							onInput={(e) => setSelectedFile(e.currentTarget.value)}
+						>
+							<option value="">{t('Select Round Backup File...')}</option>
+							<For each={roundBackups().latestFiles}>
+								{(file) => (
+									<option value={file}>
+										{file.replace(`round_backup_${props.match.id}_`, '')}
+									</option>
+								)}
+							</For>
+						</SelectInput>
+						<div class="text-center">
+							<button
+								class="btn btn-primary"
+								onClick={() => selectedFile() && mustConfirm(() => loadBackup())()}
+							>
+								{t('Load Round Backup')}
+							</button>
+						</div>
+					</>
 				)}
 			</Match>
 			<Match when={roundBackups.loading}>
