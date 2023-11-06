30f1d9faa11c7176a871df540bdaa8a4e6bed254
JensForstmann <19289807+JensForstmann@users.noreply.github.com>
2023-11-06 23:04:51 +0100

    Display players in a grid

--- frontend/src/components/PlayerListCard.tsx
+++ frontend/src/components/PlayerListCard.tsx
@@ -7,48 +7,85 @@ export const PlayerListCard: Component<{
 	match: IMatchResponse;
 }> = (props) => {
 	return (
-		<Card class="text-center">
-			<h2 class="text-lg font-bold">{t('Players')}</h2>
-			<div class="flex basis-1/2 items-start justify-center space-x-5 text-left">
-				<div class="flex-1 text-right">
-					<h4 class="text-sm font-bold">{props.match.teamA.name}</h4>
-					{List(props.match.players.filter((p) => p.team === 'TEAM_A'))}
+		<Card>
+			<h2 class="text-lg font-bold text-center">{t('Players')}</h2>
+			<div class="grid grid-cols-2 text-right gap-x-4">
+				<div class="text-right">
+					<div class="inline-grid grid-cols-[auto_auto_auto] items-baseline gap-x-2">
+						<h4 class="text-sm font-bold">{props.match.teamA.name}</h4>
+						<div></div>
+						<div></div>
+						<For each={props.match.players.filter((p) => p.team === 'TEAM_A')}>
+							{(player) => (
+								<>
+									<Player player={player} />
+									<Side player={player} />
+									<Online player={player} />
+								</>
+							)}
+						</For>
+					</div>
 				</div>
-				<div class="flex-1">
-					<h4 class="text-sm font-bold">{props.match.teamB.name}</h4>
-					{List(props.match.players.filter((p) => p.team === 'TEAM_B'))}
+				<div class="text-left">
+					<div class="inline-grid grid-cols-[auto_auto_auto] items-baseline gap-x-2">
+						<div></div>
+						<div></div>
+						<h4 class="text-sm font-bold">{props.match.teamB.name}</h4>
+						<For each={props.match.players.filter((p) => p.team === 'TEAM_B')}>
+							{(player) => (
+								<>
+									<Online player={player} />
+									<Side player={player} />
+									<Player player={player} />
+								</>
+							)}
+						</For>
+					</div>
 				</div>
-			</div>
-			<div>
-				<div>
-					<h4 class="text-sm font-bold">{t('Not Assigned')}</h4>
-					{List(props.match.players.filter((p) => !p.team))}
+				<div class="text-center col-span-2 pt-4">
+					<div class="inline-grid grid-cols-[auto_auto_auto] items-baseline gap-x-2">
+						<h4 class="text-sm font-bold col-span-3">{t('Not Assigned')}</h4>
+						<For each={props.match.players.filter((p) => !p.team)}>
+							{(player) => (
+								<>
+									<Online player={player} />
+									<Side player={player} />
+									<Player player={player} />
+								</>
+							)}
+						</For>
+					</div>
 				</div>
 			</div>
 		</Card>
 	);
 };
 
-const List = (players: IPlayer[]) => {
+const Player: Component<{ player: IPlayer }> = (props) => {
 	return (
-		<For each={players}>
-			{(player) => (
-				<>
-					<a
-						href={`https://steamcommunity.com/profiles/${player.steamId64}`}
-						target="_blank"
-					>
-						{player.name}
-					</a>{' '}
-					{player.side && <div class="badge badge-neutral">{player.side}</div>}{' '}
-					{
-						<div class="badge badge-neutral">
-							{player.online ? t('Online') : t('Offline')}
-						</div>
-					}
-					<br />
-				</>
-			)}
-		</For>
+		<div>
+			<a
+				href={`https://steamcommunity.com/profiles/${props.player.steamId64}`}
+				target="_blank"
+			>
+				{props.player.name}
+			</a>
+		</div>
+	);
+};
+
+const Side: Component<{ player: IPlayer }> = (props) => {
+	return props.player.side ? (
+		<div class="badge badge-neutral w-full">{props.player.side}</div>
+	) : (
+		<div></div>
+	);
+};
+
+const Online: Component<{ player: IPlayer }> = (props) => {
+	return (
+		<div class="badge badge-neutral w-full">
+			{props.player.online ? t('Online') : t('Offline')}
+		</div>
 	);
 };
