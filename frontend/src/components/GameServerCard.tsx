194e5da530c17f755b815f44af121673a1d045f0
JensForstmann <19289807+JensForstmann@users.noreply.github.com>
2024-04-21 16:29:15 +0200

    Remove Steam url to connect to server since it's broken for some time now (Valve, pls fix)

--- frontend/src/components/GameServerCard.tsx
+++ frontend/src/components/GameServerCard.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'solid-js';
 import { IMatchResponse } from '../../../common';
-import { SvgCopyAll, SvgOpenInNew } from '../assets/Icons';
+import { SvgCopyAll } from '../assets/Icons';
 import { t } from '../utils/locale';
 import { Card } from './Card';
 import { copyToClipboard } from '../utils/copyToClipboard';
@@ -9,7 +9,6 @@ export const GameServerCard: Component<{
 	match: IMatchResponse;
 }> = (props) => {
 	const ipPort = () => `${props.match.gameServer.ip}:${props.match.gameServer.port}`;
-	const steamUrl = () => `steam://connect/${ipPort()}/${props.match.serverPassword}`;
 	const command = () =>
 		(props.match.serverPassword ? `password "${props.match.serverPassword}"; ` : '') +
 		`connect ${ipPort()}`;
@@ -18,10 +17,6 @@ export const GameServerCard: Component<{
 		<Card class="text-center">
 			<h2 class="text-lg font-bold">{t('Game Server')}</h2>
 			<p>
-				<a href={steamUrl()}>
-					{steamUrl()} <SvgOpenInNew class="inline-block" />
-				</a>
-				<br />
 				<span class="align-middle">{command()}</span>
 				<button class="ml-1 align-middle" onClick={() => copyToClipboard(command())}>
 					<SvgCopyAll />
