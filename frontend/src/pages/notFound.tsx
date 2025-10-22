bfbf9da3d44c6fb1615c2e2915171d0b78955c3f
pixup1 <pixup1@yahoo.com>
2025-10-23 00:56:52 +0200

    Add proper 404 page

--- frontend/src/pages/notFound.tsx
+++ frontend/src/pages/notFound.tsx
@@ -1,5 +1,10 @@
 import { Component } from 'solid-js';
 
 export const NotFoundPage: Component = () => {
-	return '404';
+	return (
+		<div class="fixed inset-0 flex flex-col items-center justify-center">
+			<h1 class="text-8xl font-bold text-error">404</h1>
+			<p class="mt-2 text-2xl">Page not found</p>
+		</div>
+	);
 };
